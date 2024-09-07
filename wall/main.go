bf725ed662492a5d4003a2432bf45822a08dd111
Joseph <hejops1@gmail.com>
2024-09-07 17:09:24 +0800

    ensure mimetype of file is "image"

--- wall/main.go
+++ wall/main.go
@@ -1,18 +1,22 @@
+// Simple wallpaper setter
+
 package main
 
 import (
 	"log"
 	"math/rand/v2"
+	"mime"
 	"os"
 	"os/exec"
-	"os/user"
 	"path/filepath"
 	"strings"
 )
 
+// note: this program has the same name as /usr/bin/wall
+
 func main() {
-	u, _ := user.Current()
-	dir := filepath.Join(u.HomeDir, "wallpaper")
+	home, _ := os.UserHomeDir()
+	dir := filepath.Join(home, "wallpaper")
 
 	files := []string{}
 	err := filepath.Walk(dir, func(
@@ -35,10 +39,25 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// i could select 2 images, but i don't use 2 monitors any more
-	if exec.Command(
-		"feh", "--no-fehbg", "--bg-fill", files[rand.N(len(files))-1],
-	).Run() != nil {
-		log.Fatal(err)
+	var file string
+	for _, i := range rand.Perm(len(files)) {
+		file = files[i]
+		ext := filepath.Ext(file)
+		if mime.TypeByExtension(ext)[:5] != "image" {
+			continue
+		}
+		// i could select 2 images, but i don't use 2 monitors any more
+		if exec.Command(
+			"feh",
+			"--no-fehbg",
+			"--bg-fill",
+			// files[rand.N(len(files)-1)],
+			file,
+		).Run() != nil {
+			log.Fatal(err)
+		}
+		return
 	}
+
+	panic("no file found")
 }
