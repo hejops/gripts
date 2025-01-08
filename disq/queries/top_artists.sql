978a6854b0739e9b57ccc02853bedb459aa73f9f
Joseph <hejops1@gmail.com>
2025-01-08 20:40:57 +0100

    feat: add top_artists.sql

---
+++ disq/queries/top_artists.sql
@@ -0,0 +1,35 @@
+-- assign query result to table
+-- https://www.postgresql.org/docs/current/queries-with.html#QUERIES-WITH-SELECT
+WITH joined AS (
+    SELECT *
+    FROM albums
+    INNER JOIN albums_artists ON albums.id = albums_artists.album_id
+    INNER JOIN artists ON albums_artists.artist_id = artists.id
+    -- WHERE albums.rating >= 3
+    -- note: not sorting makes the query at least 25x slower!
+    ORDER BY albums.rating DESC
+),
+
+top_three AS (
+    SELECT
+        name,
+        title,
+        rating
+    FROM joined
+    WHERE
+        (
+            -- 'group'
+            -- https://www.machinelearningplus.com/sql/how-to-get-top-n-results-in-each-group-by-group-in-sql/
+            SELECT count(*) FROM joined AS j
+            WHERE joined.name = j.name
+        )
+        >= 3
+    ORDER BY joined.name ASC, joined.rating DESC
+)
+
+SELECT
+    name,
+    avg(rating) AS rs
+FROM top_three
+GROUP BY name
+HAVING avg(rating) >= 2.7 -- jsb ~ 2.77
